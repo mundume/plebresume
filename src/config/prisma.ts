6872301695d29b3c1af4a4c284f11050aebda4e8
nzai <nzaih18@gmail.com>
2024-01-04 14:50:12 +0300

    set the auth callback to save the user on db

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,15 +1,18 @@
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
-import dotenv from "dotenv";
-import ws from "ws";
 
-dotenv.config();
-neonConfig.webSocketConstructor = ws;
-const connectionString = `${process.env.DATABASE_URL}`;
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
+}
 
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
-const prisma = new PrismaClient({ adapter });
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
 
 export const db = prisma;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -2,6 +2,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { z } from "zod";
 import { publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { db } from "@/config/prisma";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -11,6 +12,22 @@ export const appRouter = router({
       throw new TRPCError({
         code: "UNAUTHORIZED",
       });
+
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+    if (!dbUser) {
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+          firstName: user.family_name!,
+          lastName: user.given_name!,
+        },
+      });
+    }
     return { sucess: true };
   }),
 });
