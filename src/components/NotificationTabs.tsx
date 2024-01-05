a1410a5371f0821a5de375a8738f93472d127ea7
nzai <nzaih18@gmail.com>
2024-01-05 13:40:41 +0300

    esss

--- src/components/NotificationTabs.tsx
+++ src/components/NotificationTabs.tsx
@@ -1,12 +1,29 @@
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { Button } from "./ui/button";
 import { ActivityIcon, MessageSquareWarning } from "lucide-react";
+import { Card, CardDescription, CardHeader } from "./ui/card";
 
 const tabsData = [
   {
     value: "account",
     heading: "Account",
-    content: [{ title: "Title 1", description: "Description 1" }],
+    content: [
+      {
+        title: "new job at kinde",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, eum!",
+      },
+      {
+        title: "new job at kinde",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, eum!",
+      },
+      {
+        title: "new job at kinde",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, eum!",
+      },
+    ],
   },
   {
     value: "password",
@@ -29,22 +46,22 @@ const NotificationTabs = () => {
     <Tabs defaultValue={tabsData[0].value} className="w-full px-0 mx-0">
       <TabsList className="gap-2">
         {tabsData.map((tab) => (
-          <TabsTrigger
-            key={tab.value}
-            value={tab.value}
-            className="px-4 border-b"
-          >
+          <TabsTrigger key={tab.value} value={tab.value} className="">
             {tab.heading}
           </TabsTrigger>
         ))}
       </TabsList>
       {tabsData.map((tab) => (
-        <TabsContent key={tab.value} value={tab.value}>
+        <TabsContent key={tab.value} value={tab.value} className="px-1 ">
           {tab.content.map((item) => (
-            <div key={item.title}>
-              <h3>{item.title}</h3>
-              <p>{item.description}</p>
-            </div>
+            <Card key={item.title} className="px-4 my-1">
+              <CardHeader className="p-0 py-1 text-sm font-normal ">
+                {item.title}
+              </CardHeader>
+              <CardDescription className="flex items-center justify-start pb-2 text-xs truncate">
+                {item.description}
+              </CardDescription>
+            </Card>
           ))}
         </TabsContent>
       ))}
