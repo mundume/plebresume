69453ded6e46aa649b4cbccdc4f05a4c112f74ac
nzai <nzaih18@gmail.com>
2024-01-11 06:59:24 +0300

    simplebar works now..

--- src/components/CoverLetterRenderer.tsx
+++ src/components/CoverLetterRenderer.tsx
@@ -23,31 +23,7 @@ const CoverLetterRenderer = () => {
       <SimpleBar autoHide={false} className="max-h-[calc(100vh-10rem)]">
         <div ref={ref}>
           <Document>
-            <Page size="A4" style={tw("p-12 flex flex-col")} wrap={false}>
-              <View style={tw("p-20 bg-gray-100")}>
-                <Text style={tw("text-custom text-3xl")}>Section #1</Text>
-              </View>
-              <View style={tw("mt-12 px-8 rotate-2")}>
-                <Text style={tw("text-amber-600 text-2xl")}>Section #2</Text>
-              </View>
-              <View style={tw("p-20 bg-gray-100")}>
-                <Text style={tw("text-custom text-3xl")}>Section #1</Text>
-              </View>
-              <View style={tw("mt-12 px-8 rotate-2")}>
-                <Text style={tw("text-amber-600 text-2xl")}>Section #2</Text>
-              </View>
-              <View style={tw("p-20 bg-gray-100")}>
-                <Text style={tw("text-custom text-3xl")}>Section #1</Text>
-              </View>
-              <View style={tw("mt-12 px-8 rotate-2")}>
-                <Text style={tw("text-amber-600 text-2xl")}>Section #2</Text>
-              </View>
-              <View style={tw("p-20 bg-gray-100")}>
-                <Text style={tw("text-custom text-3xl")}>Section #1</Text>
-              </View>
-              <View style={tw("mt-12 px-8 rotate-2")}>
-                <Text style={tw("text-amber-600 text-2xl")}>Section #2</Text>
-              </View>
+            <Page size="A4" style={tw("p-12 flex flex-col")}>
               <View style={tw("p-20 bg-gray-100")}>
                 <Text style={tw("text-custom text-3xl")}>Section #1</Text>
               </View>
