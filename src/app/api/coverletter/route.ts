54687c11b20429cb113661d23ced9f504e49e2d5
nzai <nzaih18@gmail.com>
2024-01-12 13:30:27 +0300

    addd sentry

--- src/app/api/coverletter/route.ts
+++ src/app/api/coverletter/route.ts
@@ -1,8 +1,11 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { openai } from "@/lib/openai";
 import { pinecone } from "@/lib/pinecone";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { coverLetterSchema } from "@/lib/validators/coverlettervalidator";
+import { db } from "@/config/prisma";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
 
 export const POST = async (req: NextRequest) => {
   const { getUser } = getKindeServerSession();
@@ -12,9 +15,34 @@ export const POST = async (req: NextRequest) => {
 
   const body = await req.json();
   const { id: fileId } = coverLetterSchema.parse(body);
-  console.log(fileId);
-  if (!user?.id)
+
+  if (!user || !user?.id)
     return new Response("Unauthorized", {
       status: 401,
     });
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileId,
+      userId: id,
+    },
+  });
+  if (!file)
+    return new Response("Not Found", {
+      status: 404,
+    });
+
+  const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
+  const pineconeIndex = pinecone.Index("plebresume");
+  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+    pineconeIndex,
+    filter: { fileId },
+  });
+
+  console.log(vectorStore);
+  return new Response("sucess", {
+    status: 200,
+  });
 };
