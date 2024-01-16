e019e879c8c3e271234f35e699f5d2718338b979
nzai <nzaih18@gmail.com>
2024-01-16 17:33:57 +0300

    everything fucked up

--- src/components/GenerateCoverLetter.tsx
+++ src/components/GenerateCoverLetter.tsx
@@ -1,30 +1,38 @@
 "use client";
 
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Button } from "./ui/button";
 import { Bot } from "lucide-react";
+import { useCompletion } from "ai/react";
 
 type Props = {
   id: string;
 };
 
 const GenerateCoverLetter = ({ id }: Props) => {
+  const { complete, input, stop, isLoading, handleInputChange, handleSubmit } =
+    useCompletion({
+      api: "/api/coverletter",
+    });
+
+  const [content, setContent] = useState("");
+
+  const checkAndPublish = useCallback(
+    async (c: string) => {
+      const completion = await complete(c);
+      if (!completion) throw new Error("Failed to check typos");
+      const typos = JSON.parse(completion);
+      // you should add more validation here to make sure the response is valid
+      if (typos?.length && !window.confirm("Typos found… continue?")) return;
+      else alert("Post published");
+    },
+    [complete]
+  );
+
   return (
-    <Button
-      className="mx-4"
-      onClick={async () => {
-        const res = await fetch(`/api/coverletter`, {
-          method: "POST",
-          body: JSON.stringify({
-            id: id,
-          }),
-        });
-        const data = await res.json();
-        console.log(data);
-      }}
-    >
+    <Button className="mx-4" onClick={() => checkAndPublish(id)}>
       <Bot className="w-4 h-4 mr-1.5 hover:shadow-2xl text-yellow-400" />
-      Generate Resume
+      Generate Resume : {isLoading ? "....." : <>completion is {content}</>}
     </Button>
   );
 };
