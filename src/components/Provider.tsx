57b8ed07751bb09ce7a49882e91b09d0c72cfeeb
nzai <nzaih18@gmail.com>
2024-01-18 07:35:57 +0300

    context lfggg!!!

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { createContext } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { ReactNode, createContext, useState } from "react";
 
 type ResumeResponse = {
   generateCoverLetter: () => void;
@@ -13,3 +14,38 @@ export const ResumeContext = createContext<ResumeResponse>({
   fileId: "",
   isLoading: false,
 });
+
+type ContextProps = {
+  fileId: string;
+  children: ReactNode;
+};
+
+export const ChatContextProvider = ({ fileId, children }: ContextProps) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { mutate: pleb } = useMutation({
+    mutationFn: async ({ fileId }: { fileId: string }) => {
+      const response = await fetch("/api/coverletter", {
+        method: "POST",
+        body: JSON.stringify({
+          fileId,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to generate cover letter");
+      }
+      return response.body;
+    },
+  });
+  const generateCoverLetter = () => pleb({ fileId });
+  return (
+    <ResumeContext.Provider
+      value={{
+        generateCoverLetter,
+        fileId,
+        isLoading,
+      }}
+    >
+      {children}
+    </ResumeContext.Provider>
+  );
+};
