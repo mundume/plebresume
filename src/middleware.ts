773f50d6eb776aeeaf8f89fbc377b9f42bb89199
nzai <nzaih18@gmail.com>
2024-02-16 12:01:15 +0300

    crying more

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,5 @@ export default function middleware(req: any) {
   return withAuth(req);
 }
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/coverletter/:path*"],
 };
