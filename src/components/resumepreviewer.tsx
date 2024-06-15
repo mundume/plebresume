2458d0aa232abee4c929b12af60e16d4ada9c391
mundume <nzaih18@gmail.com>
2024-06-15 12:00:14 +0300

    feat:fix preview

--- src/components/resumepreviewer.tsx
+++ src/components/resumepreviewer.tsx
@@ -19,28 +19,28 @@ const ResumePreviewer = () => {
   const education = educationForm.watch("education");
 
   return (
-    <div className="items-center block w-full  p-10 m-auto shadow-2xl min-h-screen">
+    <div className="items-center block w-full  p-10 m-auto shadow-2xl min-h-screen ">
       <SimpleBar autoHide={false} className="max-h-[calc(100vh-2rem)]">
         <div ref={ref} className=" font-sans text-slate-800">
           <div className="flex flex-col ">
             <div className="flex items-center justify-between w-full">
-              <h1 className="text-4xl font-bold">
+              <h1 className="text-2xl font-bold">
                 {names.firstName} {names.lastName}
               </h1>
-              <span className="p-2 text-xl border rounded-full ">NK</span>
+              <span className="p-2 border rounded-full ">NK</span>
             </div>
-            <p className="py-4 font-semibold">{proffession}</p>
+            <p className="py-4 font-semibold ">{proffession}</p>
           </div>
-          <div className="flex">
+          <div className="flex text-xs">
             <div className="flex-1">
-              <p className="flex text-sm text-slate-600">
+              <p className="flex  text-slate-600 ">
                 {" "}
                 <span className="flex-1 font-bold  text-blue-400">
                   email
                 </span>{" "}
                 <span className="flex-1">{email}</span>
               </p>
-              <p className="flex text-sm text-slate-600">
+              <p className="flex  text-slate-600 ">
                 {" "}
                 <span className="flex-1 font-bold  text-blue-400">
                   phone
@@ -49,7 +49,7 @@ const ResumePreviewer = () => {
               </p>
             </div>
             <div className="flex-1">
-              <p className="flex text-sm text-slate-600">
+              <p className="flex  text-slate-600 ">
                 <span className="flex-1 font-bold text-blue-400">Address</span>
                 <span className="flex-1">
                   {address.city}
@@ -60,16 +60,16 @@ const ResumePreviewer = () => {
           </div>
         </div>
         <div>
-          <div className="flex text-sm text-slate-600">
-            <p className="pr-5 text-xl font-bold text-slate-800 ">
+          <div className="flex text-slate-600 text-xs py-2">
+            <p className="pr-5 text-base font-bold text-slate-800 ">
               <span className="pr-1 underline text-blue-400">01</span>
               Profile
             </p>
             <p className="">{profile}</p>
           </div>
         </div>
-        <div className="py-4">
-          <p className="pr-5 text-xl font-bold text-slate-800 ">
+        <div className="">
+          <p className="pr-5 text-base font-bold text-slate-800 ">
             <span className="pr-1 underline text-blue-400">02</span>
             Work Experience
           </p>
@@ -77,17 +77,16 @@ const ResumePreviewer = () => {
             {workExperience?.map((item, index) => (
               <div
                 key={index}
-                className="grid  grid-cols-12 my-4 gap-6 items-start "
+                className="grid  grid-cols-12 my-4  items-start  "
               >
-                <div className=" col-span-3  flex  gap-2 text-xs text-blue-400 font-mono  ">
+                <div className=" col-span-3  flex  gap-2 text-xs text-blue-400   ">
                   {item.startDate && (
                     <p className=" text-xs">
                       {format(new Date(item?.startDate) || "", "MMM, yyyy")}
                     </p>
                   )}
                   -{" "}
-                  <p className=" text-xs">
-                    {" "}
+                  <p className=" text-xs ">
                     {item.currently
                       ? "Present"
                       : item.endDate &&
@@ -95,30 +94,30 @@ const ResumePreviewer = () => {
                   </p>
                 </div>
 
-                <div className=" col-span-7">
+                <div className=" col-span-7 text-xs">
                   <div
                     className="flex  gap-2 font-sans text-slate-800 items-center
                   "
                   >
                     <h2 className="font-semibold ">{item?.title}</h2>{" "}
-                    {item?.name && <p className="text-base">at</p>}
+                    {item?.name && <p className="">at</p>}
                     <h2 className="font-semibold text-blue-400">
                       {item?.name}
                     </h2>
                   </div>
-                  <Markdown className={"prose text-sm"}>
+                  <Markdown className={"prose text-xs "}>
                     {item?.description}
                   </Markdown>
                 </div>
-                <div className="text-slate-600 font-mono text-xs col-span-2">
+                <div className="text-slate-600 font-mono text-xs  col-span-2 text-center">
                   {item.location}
                 </div>
               </div>
             ))}
           </div>
         </div>
-        <div className="py-4">
-          <p className="pr-5 text-xl font-bold text-slate-800 ">
+        <div className="py-2">
+          <p className="pr-5 text-base font-bold text-slate-800 ">
             <span className="pr-1 underline text-blue-400">03</span>
             Education
           </p>
@@ -128,17 +127,14 @@ const ResumePreviewer = () => {
                 key={index}
                 className="grid  grid-cols-12 my-4 gap-6 items-start "
               >
-                <div className=" col-span-3  flex  gap-2 text-xs text-blue-400 font-mono  ">
+                <div className=" col-span-3  flex  gap-2 text-xs text-blue-400">
                   {item.startDate && (
                     <p className=" text-xs">
-                      {item.currently
-                        ? "Present"
-                        : format(new Date(item?.startDate) || "", "MMM, yyyy")}
+                      {format(new Date(item?.startDate) || "", "MMM, yyyy")}
                     </p>
                   )}
-                  -{" "}
-                  <p className=" text-xs">
-                    {" "}
+                  -
+                  <p className=" text-xs ">
                     {item.currently
                       ? "Present"
                       : item.endDate &&
@@ -146,22 +142,22 @@ const ResumePreviewer = () => {
                   </p>
                 </div>
 
-                <div className=" col-span-7">
+                <div className=" col-span-7 text-xs">
                   <div
                     className="flex  gap-2 font-sans text-slate-800 items-center
                   "
                   >
                     <h2 className="font-semibold ">{item?.title}</h2>{" "}
-                    {item?.name && <p className="text-base">at</p>}
-                    <h2 className="font-semibold text-blue-400">
+                    {item?.name && <p className="">at</p>}
+                    <h2 className="font-semibold  text-blue-400">
                       {item?.name}
                     </h2>
                   </div>
-                  <Markdown className={"prose text-sm"}>
+                  <Markdown className={"prose text-xs "}>
                     {item?.description}
                   </Markdown>
                 </div>
-                <div className="text-slate-600 font-mono text-xs col-span-2">
+                <div className="text-slate-600 font-mono text-xs col-span-2 text-center">
                   {item.location}
                 </div>
               </div>
