6cce8cb9b05f26cafc6ba8ff74805d28546dfda9
mundume <nzaih18@gmail.com>
2024-06-27 14:39:23 +0300

    fix some stuff

--- src/app/api/onedoc/route.ts
+++ src/app/api/onedoc/route.ts
@@ -21,37 +21,35 @@ export const GET = async (req: NextRequest) => {
     total: 100,
   };
   const values = {
-    values: {
-      personalInfo: {
-        names: {
-          firstName: p?.firstName,
-          lastName: p?.lastName,
-        },
-        email: p?.email,
-        phone: "0700000000",
-        proffession: "Software Engineer",
-        profile:
-          "Highly skilled UI Engineer with 5 years of experience in designing and developing intuitive user interfaces for web and mobile applications. Proficient in HTML, CSS, JavaScript, and modern front-end frameworks such as React and Vue.js. Passionate about blending design aesthetics with technical functionality to create seamless user experiences. Strong collaborator with UX designers and backend developers to deliver innovative solutions that exceed user expectations.",
-        address: {
-          city: "Kibandahasara",
-          state: "Kenya",
-        },
+    personalInfo: {
+      names: {
+        firstName: p?.firstName!,
+        lastName: p?.lastName!,
       },
-      // workExperience: [
-      //   {
-      //     companyName: "Plebs x Degens LLC",
-      //     description:
-      //       "personality hire you know just doing nothing and being lazy you know what im saying, but i cant be fired because the office will get boring without me you know what im saying",
-      //     endDate: "mar/2024",
-      //     startDate: "jan/2022",
-      //     title: "Vibes Engineer",
-      //   },
-      // ],
+      email: p?.email!,
+      phone: "0700000000",
+      proffession: "Software Engineer",
+      profile:
+        "Highly skilled UI Engineer with 5 years of experience in designing and developing intuitive user interfaces for web and mobile applications. Proficient in HTML, CSS, JavaScript, and modern front-end frameworks such as React and Vue.js. Passionate about blending design aesthetics with technical functionality to create seamless user experiences. Strong collaborator with UX designers and backend developers to deliver innovative solutions that exceed user expectations.",
+      address: {
+        city: "Kibandahasara",
+        state: "Kenya",
+      },
     },
+    // workExperience: [
+    //   {.
+    //     companyName: "Plebs x Degens LLC",
+    //     description:
+    //       "personality hire you know just doing nothing and being lazy you know what im saying, but i cant be fired because the office will get boring without me you know what im saying",
+    //     endDate: "mar/2024",
+    //     startDate: "jan/2022",
+    //     title: "Vibes Engineer",
+    //   },
+    // ],
   };
 
   const { file, error } = await onedoc.render({
-    html: await compile(Resume(values)),
+    html: await compile(Resume({ values: values })),
   });
 
   if (error) {
